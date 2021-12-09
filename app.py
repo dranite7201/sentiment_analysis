4594dd895edbaca303c444052e1bea3f11581e58
Dong <nhcd7201@gmail.com>
2021-12-09 16:22:51 +0700

    commit

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, flash, request, redirect, url_for
+import pickle
+import nltk
+app = Flask(__name__)
+
+#get classifier model
+f = open('my_classifier.pickle', 'rb')
+classifier = pickle.load(f)
+f.close()
+
+#put word inside a feature
+def word_feats(words):
+    return dict([(word, True) for word in words])
+
+
+
+@app.route('/')
+def upload_form():
+	return render_template('upload.html')
+
+
+@app.route('/result', methods=["POST","GET"])
+def classify():
+	if request.method == "POST":
+		sentence = request.form['text']
+		neg = 0
+		pos = 0
+		sw = nltk.corpus.stopwords.words('english')
+		sentence = sentence.lower()
+		sentences = sentence.split('.')  
+		tmp = []
+		classResults =[]
+		results = []
+		for sent in sentences:
+			if sent != "":
+				pos=0
+				neg=0
+				words = [word for word in sent.split(" ") if word not in sw]
+				for word in words:
+					tmp.append(word)
+					classResult = classifier.classify(word_feats(tmp))
+					if(classResult=='pos'):
+						pos+=1
+					else:
+						neg+=1
+				if (pos==neg):
+					l = [sent,"Neutral"]
+					results.append(l)
+				elif(pos>neg):
+					l = [sent,"Positive"]
+					results.append(l)
+				else:
+					l = [sent,"Negative"]
+					results.append(l)				
+		
+		return render_template('result.html', results=results)
+	return render_template('upload.html')
+
+if __name__ == "__main__":
+	app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,40 @@
+import nltk
+import pickle
+
+f = open('my_classifier.pickle', 'rb')
+classifier = pickle.load(f)
+f.close()
+
+def word_feats(words):
+    return dict([(word, True) for word in words])
+
+neg = 0
+pos = 0
+sentence = "absolutely nothing. awesome movie. bad movie ever"
+sw = nltk.corpus.stopwords.words('english')
+sentence = sentence.lower()
+sentences = sentence.split('.')  
+tmp = []
+results = []
+for sent in sentences:
+    if sent != "":
+        pos=0
+        neg=0
+        words = [word for word in sent.split(" ") if word not in sw]
+        for word in words:
+            tmp.append(word)
+            classResult = classifier.classify(word_feats(tmp))
+            if(classResult=='pos'):
+                pos+=1
+            else:
+                neg+=1
+        if (pos==neg):
+            l = [sent,"neutral"]
+            results.append(l)
+        elif(pos>neg):
+            l = [sent,"pos"]
+            results.append(l)
+        else:
+            l = [sent,"neg"]
+            results.append(l)
+print(results)
